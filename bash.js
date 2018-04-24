d4f8277c7cd841dd1b261e1edac55539c824078e
Jay.Lee <kkomarocker@gmail.com>
2018-04-24 12:48:03 -0400

    modular works

--- bash.js
+++ bash.js
@@ -1,11 +1,18 @@
+const pwd = require('./pwd')
+
 process.stdout.write('prompt >' );
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
   if (cmd === 'pwd') {
-    console.log(`${process.cwd()}`)
+    return pwd(cmd);
   } else {
-  process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('You typed: ' + cmd)
+    process.stdout.write('\nprompt >' );
   }
-  process.stdout.write('\nprompt > ');
 });
+
+
+
+
+
---
+++ pwd.js
@@ -0,0 +1,5 @@
+module.exports =  pwdFunc = (cmd) => {
+  console.log(`${process.cwd()}`)
+  process.stdout.write('\nprompt >')
+};
+
