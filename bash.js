6d20f9ac3ada258b0aae4db29e3a225e44d5703e
Kevin Delvo <kevinbdelvo@gmail.com>
2018-04-24 12:18:44 -0400

    Code: pwd

--- bash.js
+++ bash.js
@@ -2,7 +2,10 @@ process.stdout.write('prompt >' );
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-
+  if (cmd === 'pwd') {
+    console.log(`${process.cwd()}`)
+  } else {
   process.stdout.write('You typed: ' + cmd);
+  }
   process.stdout.write('\nprompt > ');
 });
